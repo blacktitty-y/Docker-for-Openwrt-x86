a7e3abb98de2579d5d5705dd155964d2d55da5d1
Blacktitty <52312781+blacktitty-y@users.noreply.github.com>
2019-06-28 16:43:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,30 @@
+#进入安装Docker挂载点（可根据自己位置更改）
 cd /mnt/sdb1
+
+#下载Docker并解压
 wget https://download.docker.com/linux/static/stable/x86_64/docker-18.09.7.tgz
 tar -xzvf docker-18.09.7.tgz
+rm -rf docker-18.09.7.tgz
+
+#将执行文件链接到/usr/sbin
 cd docker
 mkdir bin
 mv * bin
+ln -s /mnt/sdb1/docker/bin/* /usr/sbin
+
+#创建Docker相关目录与文件
 mkdir data config
 cd config
 wget https://raw.githubusercontent.com/blacktitty-y/Docker-for-Openwrt-x86/master/daemon.json
-ln -s /mnt/sdb1/docker/bin/* /usr/sbin
+
+#创建启动项
 cd /etc/init.d
 wget https://raw.githubusercontent.com/blacktitty-y/Docker-for-Openwrt-x86/master/docker
 chmod +x docker
+
+#运行Docker
 ./docker start
+
+clear
+
+echo "Docker for Openwrt x86 安装成功！"
