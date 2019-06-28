4d1d0627d14e7e13f7633abdbbab4011241970a4
Blacktitty <52312781+blacktitty-y@users.noreply.github.com>
2019-06-28 17:07:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #进入安装Docker挂载点（可根据自己位置更改）
 cd /mnt/sdb1
 
-#下载Docker并解压
+#下载Docker并解压 (此处可以自行修改最新版本下载)
 wget https://download.docker.com/linux/static/stable/x86_64/docker-18.09.7.tgz
 tar -xzvf docker-18.09.7.tgz
 rm -rf docker-18.09.7.tgz
