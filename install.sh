6d397f0c641b8d297dad4a1df27e3e7eb779208e
Blacktitty <52312781+blacktitty-y@users.noreply.github.com>
2019-06-28 17:56:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,18 +6,16 @@ wget https://download.docker.com/linux/static/stable/x86_64/docker-18.09.7.tgz
 tar -xzvf docker-18.09.7.tgz
 rm -rf docker-18.09.7.tgz
 
-#将执行文件链接到/usr/sbin
+#将执行文件链接到/usr/sbin (路径也需要改成自己的挂载点)
 cd docker
-mkdir bin
-mv * bin
-ln -s /mnt/sdb1/docker/bin/* /usr/sbin
+ln -s /mnt/sdb1/docker/* /usr/sbin
 
 #创建Docker相关目录与文件
 mkdir data config
 cd config
 wget https://raw.githubusercontent.com/blacktitty-y/Docker-for-Openwrt-x86/master/daemon.json
 
-#创建启动项并修改文件权限
+#创建启动项并修改文件权限 (docker文件内三处挂载点需要修改成自己的)
 cd /etc/init.d
 wget https://raw.githubusercontent.com/blacktitty-y/Docker-for-Openwrt-x86/master/docker
 chmod +x docker
